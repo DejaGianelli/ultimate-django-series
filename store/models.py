4bd9852e2a0a6084636b1d138ae2513cb845482e
Dejamir Gianelli <dejamir.gianelli@gmail.com>
2022-09-15 23:09:18 -0300

    Section: Django ORM
    Lesson: Resetting the Database.

--- store/models.py
+++ store/models.py
@@ -54,7 +54,7 @@ class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.PROTECT)
     product = models.ForeignKey(Product, on_delete=models.PROTECT)
     quantity = models.PositiveSmallIntegerField()
-    unit_price = models.DecimalField(max_digits=2, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
 
 class Address(models.Model):
     street = models.CharField(max_length=255)
