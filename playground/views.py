bea82fdff4c2acef6d0b093430fe5a820cffea3c
Dejamir Gianelli <dejamir.gianelli@gmail.com>
2022-09-17 00:25:09 -0300

    Section: Django ORM
    Lesson: Aggregating Objects

--- playground/views.py
+++ playground/views.py
@@ -3,25 +3,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Q, F
+from django.db.models.aggregates import Count, Max, Min, Avg, Sum
 from store.models import Product, OrderItem, Order
 
 
 def say_hello(request):
    
-   # queryset = Product.objects.select_related('collection').all()
-   
-   # queryset = Product.objects.prefetch_related('promotions').all()
-   
-   # queryset = Product.objects.prefetch_related('promotions') \
-   #    .select_related('collection') \
-   #    .all()
-   
-   queryset = Order.objects \
-      .prefetch_related('orderitem_set__product') \
-      .select_related('customer') \
-      .order_by('-placed_at')[:5]
+   # result = Product.objects.aggregate(count=Count('id'))
+   result = Product.objects.filter(collection__id=3).aggregate(
+      count=Count('id'), min_price=Min('unit_price'))
       
    return render(request, 'hello.html', {
       'name': 'Mosh', 
-      'orders': list(queryset)
+      'result': result
    })
