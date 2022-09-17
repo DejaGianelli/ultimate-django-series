71d661911e53432a3f7e1cffe07ae90d0a301ba5
Dejamir Gianelli <dejamir.gianelli@gmail.com>
2022-09-16 23:43:48 -0300

    Section: Django ORM
    Lesson: Selecting Related Objects

--- playground/views.py
+++ playground/views.py
@@ -3,16 +3,25 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Q, F
-from store.models import Product, OrderItem
+from store.models import Product, OrderItem, Order
 
 
 def say_hello(request):
    
-   # Be careful with this methods, it can trigger a lot of queries
-   # queryset = Product.objects.only('id', 'title')
-   queryset = Product.objects.defer('description')
+   # queryset = Product.objects.select_related('collection').all()
+   
+   # queryset = Product.objects.prefetch_related('promotions').all()
+   
+   # queryset = Product.objects.prefetch_related('promotions') \
+   #    .select_related('collection') \
+   #    .all()
+   
+   queryset = Order.objects \
+      .prefetch_related('orderitem_set__product') \
+      .select_related('customer') \
+      .order_by('-placed_at')[:5]
       
    return render(request, 'hello.html', {
       'name': 'Mosh', 
-      'products': list(queryset)
+      'orders': list(queryset)
    })
