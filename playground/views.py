ded7f3a6106a31d2f01366f5ef02791158c8980a
Dejamir Gianelli <dejamir.gianelli@gmail.com>
2022-09-16 22:14:07 -0300

    Section: Django ORM
    Lesson: Filtering Objects

--- playground/views.py
+++ playground/views.py
@@ -6,10 +6,18 @@ from store.models import Product
 
 
 def say_hello(request):
-   try:
-      product = Product.objects.get(pk=1)
-      print(product)
-   except ObjectDoesNotExist:
-      pass
    
-   return render(request, 'hello.html', {'name': 'Mosh'})
+   # queryset = Product.objects.filter(unit_price=20)
+   # queryset = Product.objects.filter(unit_price__gt=20)
+   # queryset = Product.objects.filter(unit_price__range=(20, 30))
+   # queryset = Product.objects.filter(collection__id__in=[1, 2, 3])
+   # queryset = Product.objects.filter(title__icontains='coffee')
+   # queryset = Product.objects.filter(title__istartswith='coffee')
+   # queryset = Product.objects.filter(title__iendswith='coffee')
+   # queryset = Product.objects.filter(last_update__year=2021)
+   queryset = Product.objects.filter(description__isnull=True)
+   
+   return render(request, 'hello.html', {
+      'name': 'Mosh', 
+      'products': list(queryset)
+   })
