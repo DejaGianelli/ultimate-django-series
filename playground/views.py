2dbfe9b2df3eb9f7942e70ee79ef4a76f80cedf9
Dejamir Gianelli <dejamir.gianelli@gmail.com>
2022-09-16 22:28:04 -0300

    Section: Django ORM
    Lesson: Referencing Fields using F Objects

--- playground/views.py
+++ playground/views.py
@@ -2,17 +2,13 @@ import re
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
-from django.db.models import Q
+from django.db.models import Q, F
 from store.models import Product
 
 
 def say_hello(request):
    
-   # queryset = Product.objects.filter(inventory__lt=10, unit_price__lt=20)
-   # queryset = Product.objects.filter(inventory__lt=10) \
-      # .filter(unit_price__lt=20)
-   # queryset = Product.objects.filter(Q(inventory__lt=10) | Q(unit_price__lt=20))
-   queryset = Product.objects.filter(Q(inventory__lt=10) & ~Q(unit_price__lt=20))
+   queryset = Product.objects.filter(inventory=F('unit_price'))
    
    return render(request, 'hello.html', {
       'name': 'Mosh', 
