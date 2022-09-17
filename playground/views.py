da43294fb6964b508d9c25cf2883d5b4ada05f3f
Dejamir Gianelli <dejamir.gianelli@gmail.com>
2022-09-17 13:55:36 -0300

    Section: Django ORM
    Lesson: Annotating Objects

--- playground/views.py
+++ playground/views.py
@@ -2,18 +2,17 @@ import re
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
-from django.db.models import Q, F
+from django.db.models import Q, F, Value
 from django.db.models.aggregates import Count, Max, Min, Avg, Sum
-from store.models import Product, OrderItem, Order
+from store.models import Product, OrderItem, Order, Customer
 
 
 def say_hello(request):
    
-   # result = Product.objects.aggregate(count=Count('id'))
-   result = Product.objects.filter(collection__id=3).aggregate(
-      count=Count('id'), min_price=Min('unit_price'))
+   # queryset = Customer.objects.annotate(is_new=Value(True))
+   queryset = Customer.objects.annotate(new_id=F('id') + 1)
       
    return render(request, 'hello.html', {
       'name': 'Mosh', 
-      'result': result
+      'result': list(queryset)
    })
