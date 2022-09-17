fe3af9e5af6d49b50739875704e427c7dbbb7caa
Dejamir Gianelli <dejamir.gianelli@gmail.com>
2022-09-16 22:25:08 -0300

    Section: Django ORM
    Lesson: Complex Lookups Using Q Objects.

--- playground/views.py
+++ playground/views.py
@@ -2,20 +2,17 @@ import re
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Q
 from store.models import Product
 
 
 def say_hello(request):
    
-   # queryset = Product.objects.filter(unit_price=20)
-   # queryset = Product.objects.filter(unit_price__gt=20)
-   # queryset = Product.objects.filter(unit_price__range=(20, 30))
-   # queryset = Product.objects.filter(collection__id__in=[1, 2, 3])
-   # queryset = Product.objects.filter(title__icontains='coffee')
-   # queryset = Product.objects.filter(title__istartswith='coffee')
-   # queryset = Product.objects.filter(title__iendswith='coffee')
-   # queryset = Product.objects.filter(last_update__year=2021)
-   queryset = Product.objects.filter(description__isnull=True)
+   # queryset = Product.objects.filter(inventory__lt=10, unit_price__lt=20)
+   # queryset = Product.objects.filter(inventory__lt=10) \
+      # .filter(unit_price__lt=20)
+   # queryset = Product.objects.filter(Q(inventory__lt=10) | Q(unit_price__lt=20))
+   queryset = Product.objects.filter(Q(inventory__lt=10) & ~Q(unit_price__lt=20))
    
    return render(request, 'hello.html', {
       'name': 'Mosh', 
