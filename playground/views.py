e5bf5cdf2e375cab6e49eb9bdaa381a9230089df
Dejamir Gianelli <dejamir.gianelli@gmail.com>
2022-09-17 15:22:08 -0300

    Section: Django ORM
    Lesson: Transactions

--- playground/views.py
+++ playground/views.py
@@ -6,17 +6,43 @@ from django.db.models import Q, F, Value, Func, ExpressionWrapper, DecimalField
 from django.db.models.aggregates import Count, Max, Min, Avg, Sum
 from django.db.models.functions import Concat
 from django.contrib.contenttypes.models import ContentType
+from django.db import transaction
 from store.models import Collection, Product, OrderItem, Order, Customer
 from tags.models import TaggedItem
 
-
+# @transaction.atomic
+# def say_hello(request):
+   
+#    order = Order()
+#    order.customer_id = 1
+#    order.save()
+   
+#    item = OrderItem()
+#    item.order = order
+#    item.product_id = 1
+#    item.quantity = 1
+#    item.unit_price = 10
+#    item.save()
+   
+#    return render(request, 'hello.html', {
+#       'name': 'Mosh'
+#    })
 
 def say_hello(request):
    
-   # collection = Collection(pk=11)
-   # collection.delete()
+   # ...
    
-   Collection.objects.filter(id__gt=5).delete()
+   with transaction.atomic():
+      order = Order()
+      order.customer_id = 1
+      order.save()
+      
+      item = OrderItem()
+      item.order = order
+      item.product_id = -1
+      item.quantity = 1
+      item.unit_price = 10
+      item.save()
    
    return render(request, 'hello.html', {
       'name': 'Mosh'
