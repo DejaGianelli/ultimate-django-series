3741c9dda8ecff0ffb31712d1aaa798e3fa56625
Dejamir Gianelli <dejamir.gianelli@gmail.com>
2022-09-17 14:45:15 -0300

    Section: Django ORM
    Lesson: Querying Generic Relationships

--- playground/views.py
+++ playground/views.py
@@ -5,16 +5,19 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Q, F, Value, Func, ExpressionWrapper, DecimalField
 from django.db.models.aggregates import Count, Max, Min, Avg, Sum
 from django.db.models.functions import Concat
+from django.contrib.contenttypes.models import ContentType
 from store.models import Product, OrderItem, Order, Customer
+from tags.models import TaggedItem
+
 
 
 def say_hello(request):
    
-   discounted_price = ExpressionWrapper(F('unit_price') * 0.8, output_field=DecimalField())
-   queryset = Product.objects.annotate(
-      discounted_price=discounted_price
-   )
-      
+   content_type = ContentType.objects.get_for_model(Product)
+   queryset = TaggedItem.objects \
+      .select_related('tag') \
+      .filter(content_type=content_type, object_id=5)
+   
    return render(request, 'hello.html', {
       'name': 'Mosh', 
       'result': list(queryset)
