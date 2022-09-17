e3e07efaede1c0d6dd064fc7827e289108e0f692
Dejamir Gianelli <dejamir.gianelli@gmail.com>
2022-09-17 18:32:49 -0300

    Section: The Admin Site
    Lesson: Registering Models

--- store/models.py
+++ store/models.py
@@ -7,6 +7,13 @@ class Promotion(models.Model):
 class Collection(models.Model):
     title = models.CharField(max_length=255)
     featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='+')
+    
+    def __str__(self) -> str:
+        return self.title
+    
+    class Meta:
+        ordering = ['title']
+    
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
@@ -17,6 +24,12 @@ class Product(models.Model):
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
     promotions = models.ManyToManyField(Promotion, related_name='products')
+    
+    def __str__(self) -> str:
+        return self.title
+    
+    class Meta:
+        ordering = ['title']
     
 class Customer(models.Model):
     MEMBERSHIP_BRONZE = 'B'
