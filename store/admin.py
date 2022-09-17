33bc8cd934b53aae2487f43907ddd1875fc3ef86
Dejamir Gianelli <dejamir.gianelli@gmail.com>
2022-09-17 20:25:44 -0300

    Section: The Admin Site
    Lesson: Using Generic Relations

--- store/admin.py
+++ store/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin, messages
+from django.contrib.contenttypes.admin import GenericTabularInline
 from django.db.models.aggregates import Count
 from django.utils.html import format_html, urlencode
 from django.urls import reverse
+
+from tags.models import TaggedItem
 from . import models
 
 class InventoryFilter(admin.SimpleListFilter):
@@ -18,8 +21,13 @@ class InventoryFilter(admin.SimpleListFilter):
             return queryset.filter(inventory__lt=10)
     
 
+class TagInline(GenericTabularInline):
+    autocomplete_fields = ['tag']
+    model = TaggedItem
+
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
+    inlines = [TagInline]
     autocomplete_fields = ['collection']
     search_fields = ['title']
     prepopulated_fields = {
