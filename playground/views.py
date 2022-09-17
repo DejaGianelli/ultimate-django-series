c9f95b2935c4122327befb0d8a0800bb6f7a3316
Dejamir Gianelli <dejamir.gianelli@gmail.com>
2022-09-17 14:10:38 -0300

    Section: Django ORM
    Lesson: Grouping Data

--- playground/views.py
+++ playground/views.py
@@ -10,12 +10,8 @@ from store.models import Product, OrderItem, Order, Customer
 
 def say_hello(request):
    
-   # queryset = Customer.objects.annotate(is_new=Value(True))
-   # queryset = Customer.objects.annotate(
-   #    full_name=Func(F('first_name'), Value(' '), F('last_name'), function='CONCAT')
-   # )
    queryset = Customer.objects.annotate(
-      full_name=Concat('first_name', Value(' '), 'last_name')
+      orders_count=Count('order')
    )
       
    return render(request, 'hello.html', {
