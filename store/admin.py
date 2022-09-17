7a9c028a57fe0b950b2da272a5fda87d550613e1
Dejamir Gianelli <dejamir.gianelli@gmail.com>
2022-09-17 19:08:55 -0300

    Section: The Admin Site
    Lesson: Providing Links to Other Pages

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.db.models.aggregates import Count
+from django.utils.html import format_html, urlencode
+from django.urls import reverse
 from . import models
 
 @admin.register(models.Product)
@@ -34,7 +36,14 @@ class CollectionAdmin(admin.ModelAdmin):
     
     @admin.display(ordering='products_count')
     def products_count(self, collection):
-        return collection.products_count
+        url = (
+            reverse('admin:store_product_changelist') 
+            + '?'
+            + urlencode({
+                'collection__id': str(collection.id)
+            })
+        )
+        return format_html('<a href="{}">{}</a>', url, collection.products_count)
     
     def get_queryset(self, request):
         return super().get_queryset(request).annotate(
