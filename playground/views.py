a3e3e5298d1bd38585fa2265d0f35ebfd511c785
Dejamir Gianelli <dejamir.gianelli@gmail.com>
2022-09-16 23:03:08 -0300

    Section: Django ORM
    Lesson: Selecting fields to query

--- playground/views.py
+++ playground/views.py
@@ -3,21 +3,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Q, F
-from store.models import Product
+from store.models import Product, OrderItem
 
 
 def say_hello(request):
    
-   # queryset = Product.objects.order_by('title')
-   # queryset = Product.objects.order_by('unit_price', '-title')
-   # queryset = Product.objects.order_by('unit_price', '-title').reverse()
-   # queryset = Product.objects.filter(collection__id=3).order_by('unit_price')
-   # product = Product.objects.order_by('unit_price')[0]
-   # product = Product.objects.earliest('unit_price')
-   # product = Product.objects.latest('unit_price')
-   # queryset = Product.objects.all()[:5]
-   queryset = Product.objects.all()[5:10]
-   
+   # queryset = Product.objects.values('id', 'title', 'collection__title')
+   # queryset = Product.objects.values_list('id', 'title', 'collection__title')
+   queryset =  Product.objects \
+      .filter(id__in=OrderItem.objects.values('product_id').distinct()) \
+      .order_by('title') 
+      
+      
    return render(request, 'hello.html', {
       'name': 'Mosh', 
       'products': list(queryset)
