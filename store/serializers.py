ce9ec92b203497842e396b46d39a26c938f168cc
Dejamir Gianelli <dejamir.gianelli@gmail.com>
2022-09-18 14:31:45 -0300

    Part 2
    Section: Building RESTful APIs with Django REST Framework
    
    Lesson: Serializing Objects

--- store/serializers.py
+++ store/serializers.py
@@ -2,6 +2,6 @@ from rest_framework import serializers
 
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
-    title = serializers.CharField(max_lenght=255)
+    title = serializers.CharField(max_length=255)
     unit_price = serializers.DecimalField(max_digits=6, decimal_places=2)
     
