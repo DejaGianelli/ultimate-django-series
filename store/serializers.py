648ed0e1a6c4c59793742b05cdcc78a847d94e57
Dejamir Gianelli <dejamir.gianelli@gmail.com>
2022-09-18 16:02:45 -0300

    Part 2
    Section: Building RESTful APIs with Django REST Framework
    
    Lesson: Saving Objects

--- store/serializers.py
+++ store/serializers.py
@@ -6,19 +6,27 @@ class CollectionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Collection
         fields = ['id', 'title']
-        
-    # id = serializers.IntegerField()
-    # title = serializers.CharField(max_length=255)
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['id', 'title', 'unit_price', 'price_with_tax', 'collection']
+        fields = ['id', 'title', 'description', 'slug', 'inventory', 'unit_price', 'price_with_tax', 'collection']
         
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
     
     def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
+    
+    # def create(self, validated_data):
+    #     product = Product(**validated_data)
+    #     product.other = 1
+    #     product.save()
+    #     return product
+    
+    # def update(self, instance,  validated_data):
+    #     instance.unit_price = validated_data.get('unit_price')
+    #     instance.save()
+    #     return instance
     
     # def validate(self, data):
     #     if data['password'] != data['confirm_password']:
